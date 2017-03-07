eba2ec2fb724784db4b0819a9e463fc7e76ddfec
sbayram70 <sbayram700@gmail.com>
2017-03-07 12:06:50 +0100

    added

---
+++ instapopdracht page.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page</title>
+</head>
+<body>
+<form action="">
+	<input type="text" name="getal">
+  	<input type="submit" value="Submit">
+</form>
+</body>
+</html>
---
+++ instapopdracht.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript">
+
+for(i=0;i<10;)console.log(k=i/2|7,"*",l=i%10+1,"=",k*l,++i%1?"":"\n")
+
+  </script>
+</head>
+<body>
+
+
+</body>
+</html>
